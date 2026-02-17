771c0f9a4cb2ce2314aeffa70c525a92d2827473
unknown <email.recuperoxearth@gmail.com>
2026-02-17 14:40:15 +0100

    added image loading

--- src/app.js
+++ src/app.js
@@ -1,42 +1,68 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const multer = require("multer");
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 
+// Configurazione upload
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.join(__dirname, "..", "uploads"));
+    },
+    filename: function (req, file, cb) {
+        const ext = path.extname(file.originalname); // prende .jpg/.png
+        const uniqueName = Date.now() + ext;         // nome unico
+        cb(null, uniqueName);
+    }
+});
+
+const upload = multer({ storage: storage });
+
+
+
 // Render pagina /post
 app.get('/post', (req, res) => {
     res.send(`
-        <form method="POST" action="/post">
-            <input type="text" name="title" placeholder="Titolo" required />
-            <button type="submit">Salva</button>
+        <form action="/post" method="POST" enctype="multipart/form-data">
+            <input type="text" name="title" placeholder="Titolo" />
+            <textarea name="description" placeholder="Descrizione"></textarea>
+            <input type="file" name="image" />
+            <button type="submit">Invia</button>
         </form>
     `);
 });
 
 // Salvataggio su post.json
-app.post('/post', (req, res) => {
-    const filePath = path.join(__dirname, '/jsons/post.json');
+app.post('/post', upload.single("image"), (req, res) => {
+    const filePath = path.join(__dirname, 'jsons', 'post.json');
     let data = [];
 
     if (fs.existsSync(filePath)) {
-        data = JSON.parse(fs.readFileSync(filePath));
+        const fileContent = fs.readFileSync(filePath, "utf-8");
+        data = fileContent ? JSON.parse(fileContent) : [];
     }
 
-    data.push({ title: req.body.title });
+    data.push({ 
+        title: req.body.title,
+        description: req.body.description,
+        image: req.file ? req.file.filename : null
+    });
 
     fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
 
     res.send("Salvato su post.json");
 });
 
+// Rendi pubblica la cartella uploads
+app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
+
 app.listen(PORT, () => {
     console.log(`Server avviato su http://localhost:${PORT}`);
 });
-
 
 // Fallback (404)
 app.use((req, res) => {
